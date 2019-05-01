e6e12d2cd234398ba9499a281d4c483175be4cf3
Nico <nmrainhart@gmail.com>
2019-05-01 19:07:47 -0300

    Fix failing test

--- app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/MainActivity.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/MainActivity.kt
@@ -1,10 +1,8 @@
 package ar.edu.utn.frba.mobile.a2019c1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.widget.ImageButton
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,32 +11,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        supportActionBar!!.setTitle("Conversor de Unidades de temperatura")
+        supportActionBar!!.title = "Conversor de Unidades de temperatura"
 
         val conversor = ConversorUnidades()
-        //val gradosCelsius = temperaturaIngresadaC
 
+        botonCelsius.isEnabled = false
+        temperaturaIngresadaC.doOnTextChanged { _, _, _, _ ->
+            botonCelsius.isEnabled = temperaturaIngresadaC.text.isNotBlank()
+        }
 
-        val botonC = findViewById(R.id.botonCelsius) as ImageButton
-        botonC.setOnClickListener {
+        botonCelsius.setOnClickListener {
             // Llamo a celsiustof y celsius to k
             val valorC = temperaturaIngresadaC.text.toString().toDouble()
             temperaturaIngresadaF.setText(conversor.celsiusToF(valorC).toString())
             temperaturaIngresadaK.setText(conversor.celsiusToK(valorC).toString())
         }
 
-        val botonK = findViewById(R.id.botonKelvin) as ImageButton
-
-        botonK.setOnClickListener {
+        botonKelvin.setOnClickListener {
             // Llamo a kelvintoc y kelvintof
             val valorK = temperaturaIngresadaK.text.toString().toDouble()
             temperaturaIngresadaC.setText(conversor.kelvinToC(valorK).toString())
             temperaturaIngresadaF.setText(conversor.kelvinToF(valorK).toString())
         }
 
-        val botonF = findViewById(R.id.botonFahrenheit) as ImageButton
-
-        botonF.setOnClickListener {
+        botonFahrenheit.setOnClickListener {
             // Llamo a fahtoc y fahtok
             val valorF = temperaturaIngresadaF.text.toString().toDouble()
             temperaturaIngresadaC.setText( conversor.fahToC(valorF).toString())
